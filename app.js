2a895346d4c714b619635598117f7d59a69d2a0b
Jethanandj52 <134337163+Jethanandj52@users.noreply.github.com>
2024-09-27 07:13:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,106 @@
+
+// Question 1
+
+// var checkWords= prompt("Question No:1 \nEnter any Character ")
+
+// let ascii = checkWords.charCodeAt(0); 
+
+// if(ascii>=65 && ascii<=90){
+//     alert("The entered word is a Upercase");
+// }
+// else if(ascii>=97 && ascii<=122){
+//     alert("The entered word is a lower");
+// }
+// else{
+//     alert("The entered word is not a valid Character");
+// }
+
+// Question 2
+
+
+// var firstNumber=Number(prompt("Question No:2 \nEnter First Number:"))
+// var secondNumber=Number(prompt("Enter Second Number:"))
+
+// if(firstNumber>secondNumber){
+//     alert(firstNumber+" is greater than "+secondNumber)
+// }
+// else if (firstNumber<secondNumber){
+//     alert(secondNumber+" is greater than "+firstNumber)
+// }
+// else(
+//     alert(firstNumber+" is equal to "+secondNumber)
+// )
+
+// Question 3
+
+// var number=Number(prompt("Question No:3 \nEnter First Number:"))
+ 
+
+// if(number>0){
+//     alert("This number is possitive: "+number)
+// }
+// else if (number<0){
+//     alert("This Number is Nagative: "+number)
+     
+// }
+// else(
+//     alert("This number is equal to:"+number)
+// )
+
+// Question 4
+// var character=prompt(" Question No:4 \n Enter any character").toLocaleLowerCase();
+
+// if(character=='a'|| character=='e'|| character=='i'|| character=='o'|| character=='u'){
+//     alert("The entered character is a vowel: "+character);
+// }
+// else if(character!=='a'|| character!=='e'|| character!=='i'|| character!=='o'|| character!=='u'){
+//     alert("The entered character is a consonant: "+character);
+
+// }
+// else{
+//     alert("The entered character is not a valid character");
+// }
+ 
+//Question 5
+// var password=Number(prompt("Question No:5 \n Enter your Password"))
+// var number=123;
+// if(password==number){
+//     alert("Your password is correct")
+// }
+// else{
+//     alert("Your password is incorrect")
+// }
+//Question 6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = " Question no:6\n Good day";
+// alert(greeting)
+// }
+// else{
+// greeting = "Question no:6\n Good evening";
+// alert(greeting)
+// }
+
+
+//Question 7
+ 
+// var time =Number(prompt("Question No:7 \n Enter any Clock Number "))
+// if (time >=0 && time<12) {
+ 
+// alert("Good Morning..!")
+// }
+// else if(time>=12 && time<17){
+ 
+// alert("Good Afternoon..!")
+// }
+// else if(time >=17 && time<21){
+  
+//     alert("Good Evening..!")
+// }
+// else if(time >=21 && time<=24){
+//     alert("Good Night..!")
+// }
+// else{
+//     alert("Invalid Time")
+// }
